a13d9869bae9890f1a469b848584da22112fa6aa
Gabriel Pereira dos Santos <bielsantossk8@gmail.com>
2021-04-03 15:10:00 -0300

    Icone historico
    TA TOP

--- Projeto/historico.php
+++ Projeto/historico.php
@@ -3,9 +3,12 @@
 <head>
   <?php include "./components/head.php" ?>
   <title>SRV | Histórico</title>
+  
 </head>
 <body>
   <div class="container">
+  <i class="material-icons">history</i>
+
     <h1>Histórico</h1>
   </div>
 </body>
