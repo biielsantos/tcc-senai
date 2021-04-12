145fd8fe43bae913a19dbc4345d446e3caa68046
Luís Galete <61747990+yunger7@users.noreply.github.com>
2021-04-12 10:15:53 -0300

    Insecao da navbar nas paginas

--- Projeto/veiculos.php
+++ Projeto/veiculos.php
@@ -1,12 +1,19 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <?php include "./components/head.php" ?>
+  <link rel="stylesheet" href="./styles/navbar.css">
   <title>SRV | Veículos</title>
 </head>
 <body>
+  <?php include "./components/navbar.php" ?>
   <div class="container">
     <h1>Consultar veículos</h1>
   </div>
 </body>
+<?php include ("./components/scripts.php") ?>
 </html>
