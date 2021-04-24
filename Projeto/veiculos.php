b03509e2eee866bb763913d88a6879d050ff0468
Luís Galete <61747990+yunger7@users.noreply.github.com>
2021-04-24 16:43:47 -0300

    Adicao dos inputs e modal

--- Projeto/veiculos.php
+++ Projeto/veiculos.php
@@ -58,7 +58,7 @@
             `
           }
         },
-        height: 700,
+        height: 550,
         nowIndicator: true,
         headerToolbar: {
           start: "today prev,next",
@@ -95,9 +95,88 @@
 <body>
   <?php include "./components/navbar.php" ?>
   <div class="container">
-    <h1>Consultar veículos</h1>
+    <h2>Consultar veículos</h2>
     <div id='calendar'></div>
+    <form class="reserva">
+      <div class="date-inputs">
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="dataSaida" type="date" class="validate" required>
+            <label for="dataSaida">Data saída</label>
+          </div>
+          <div class="input-field col s6">
+            <input id="dataRetorno" type="date" class="validate" required>
+            <label for="dataRetorno">Data retorno</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="horarioSaida" type="time" class="validate" required>
+            <label for="horarioSaida">Horário Saída</label>
+          </div>
+          <div class="input-field col s6">
+            <input id="horarioRetorno" type="time" class="validate" required>
+            <label for="horarioRetorno">Horário Retorno</label>
+          </div>
+        </div>
+      </div>
+      <div class="selecionar-veiculo">
+        <div class="input-field">
+          <select required>
+            <option value="" disabled selected>Selecione um veículo</option>
+            <?php
+              include "./config/conexao.php";
+              $sql = "SELECT id_veiculo, modelo FROM veiculo";
+              $res = mysqli_query($conn, $sql);
+              $veiculos = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        
+              mysqli_free_result($res);
+              mysqli_close($conn);
+            ?>
+
+            <?php foreach ($veiculos as $veiculo) { ?>
+              <option value="<?php echo $veiculo["id_veiculo"] ?>"><?php echo $veiculo["modelo"] ?></option>
+            <?php } ?>
+
+          </select>
+          <label>Veículo</label>
+        </div>
+        <a class="btn waves-effect waves-light modal-trigger" href="#modal-reserva">Reservar
+          <i class="material-icons left">done</i>
+        </a>
+      </div>
+
+      <!-- Modal -->
+      <div id="modal-reserva" class="modal">
+        <div class="modal-content">
+          <div class="container">
+            <header>
+              <h4>Finalizar reserva</h4>
+              <i class="material-icons">directions_car</i>
+            </header>
+            <div class="input-field">
+              <input id="destino" type="text" name="destino" required>
+              <label for="destino">Destino</label>
+            </div>
+            <div class="input-field">
+              <input id="responsavel" type="text" name="responsavel" required>
+              <label for="responsavel">Responsável</label>
+            </div>
+            <div class="input-field">
+              <input id="usuario" type="text" name="usuario" required>
+              <label for="usuario">Usuário</label>
+            </div>
+          </div>
+          </div>
+        <div class="modal-footer">
+          <button type="submit" class="waves-effect waves-light btn">
+            Confirmar
+          </button>
+        </div>
+      </div>
+    </form>
   </div>
 </body>
 <?php include ("./components/scripts.php") ?>
+<script src="./scripts/consultar.js"></script>
 </html>
