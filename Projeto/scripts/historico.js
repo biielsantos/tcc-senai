63416ad4d29fa843151980052271476e4ab03f14
Gabriel Pereira dos Santos <bielsantossk8@gmail.com>
2021-04-26 13:31:15 -0300

    Adição Histórico datatables
    Histórico atualizado com sucesso 👌🤙

---
+++ Projeto/scripts/historico.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#historico').DataTable({
+    "language":{
+      "url": "https://cdn.datatables.net/plug-ins/1.10.24/i18n/Portuguese-Brasil.json"
+    }
+  });
+});
