c44e39f507d655d8011727aa6fc07ba75ab00b83
Luís Galete <luisgalete6264@gmail.com>
2021-05-14 13:31:13 -0300

    Adicao botao 'Reservar' na navbar

--- Projeto/components/navbar.php
+++ Projeto/components/navbar.php
@@ -1,6 +1,13 @@
 <nav>
   <div class="nav-wrapper">
-    <a href="#" data-target="slide-out" class="sidenav-trigger show-on-large btn-floating btn-flat waves-effect waves-light"><i class="material-icons">menu</i></a>
+    <ul class="quick-access">
+      <li>
+        <a href="#" data-target="slide-out" class="sidenav-trigger show-on-large btn-floating btn-flat waves-effect waves-light"><i class="material-icons">menu</i></a>
+      </li>
+      <li>
+        <a href="/tcc/veiculos.php" class="waves-effect waves-light btn-flat white-text reservar"><i class="material-icons right">local_shipping</i>Reservar</a>
+      </li>
+    </ul>
     <div class="usuario">
       <span class="nome"><?php echo $_SESSION['nome']; ?></span>
       <i class="material-icons">account_circle</i>
@@ -16,7 +23,7 @@
     </li>
     <li><div class="divider"></div></li>
     <li><a class="subheader">Acesso Rápido</a></li>
-    <li><a href="/tcc/veiculos.php" class="waves-effect"><i class="material-icons"">add</i>Reservar Veículo</a></li>
+    <li><a href="/tcc/veiculos.php" class="waves-effect"><i class="material-icons"">add</i>Reservas</a></li>
     <li><a href="/tcc/historico.php" class="waves-effect"><i class="material-icons"">history</i>Histórico</a></li>
     <?php if ($_SESSION['tipo'] === "A") { ?>
       <li><div class="divider"></div></li>
