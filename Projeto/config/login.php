340329abed50ee91fe8de29a60ab9d12c3ff123b
Luís Galete <luisgalete6264@gmail.com>
2021-05-21 11:49:37 -0300

    Avisos de login invalido & CPF character counter

--- Projeto/config/login.php
+++ Projeto/config/login.php
@@ -1,20 +1,40 @@
 <?php
-//Iniciar uma sessão
-session_start();
-include("./conexao.php");
-$cpf = mysqli_escape_string($conn, $_POST['cpf']);
-$senha = mysqli_escape_string($conn, base64_encode($_POST['senha']));
-$query = mysqli_query($conn, "SELECT * FROM usuario WHERE cpf = '$cpf' AND senha = '$senha'") or die(mysqli_connect_error());
-if (mysqli_num_rows($query) > 0) {
-    $usuario = mysqli_fetch_array($query);
-    $_SESSION['id'] = $usuario['id_usuario'];
-    $_SESSION['nome'] = $usuario['nome'];
-    $_SESSION['cpf'] = $usuario['cpf'];
-    $_SESSION["tipo"] = $usuario['tipo'];
-    $_SESSION["status"] = "ok";
-    header('Location: ../veiculos.php');
-} else {
-    echo 'usuario Invalido';
+
+if (isset($_POST["cpf"]) && isset($_POST["senha"]) && isset($_POST["option"])) {
+	include("./conexao.php");
+
+	$cpf = mysqli_escape_string($conn, $_POST['cpf']);
+	$senha = mysqli_escape_string($conn, base64_encode($_POST['senha']));
+	$option = $_POST["option"];
+
+	$query = mysqli_query($conn, "SELECT * FROM usuario WHERE cpf = '$cpf' AND senha = '$senha'") or die(mysqli_connect_error());
+	
+	switch ($option) {
+		case "VERIFY":
+			if (mysqli_num_rows($query) > 0) {
+				$return = "ok";
+			} else {
+				$return = "invalid";
+			}
+			break;
+		case "LOGIN":
+			if (mysqli_num_rows($query) > 0) {
+				session_start();
+
+				$usuario = mysqli_fetch_array($query);
+
+				$_SESSION['id'] = $usuario['id_usuario'];
+				$_SESSION['nome'] = $usuario['nome'];
+				$_SESSION['cpf'] = $usuario['cpf'];
+				$_SESSION["tipo"] = $usuario['tipo'];
+				$_SESSION["status"] = "ok";
+				
+				header('Location: ../veiculos.php');
+			}
+			break;
+	}
+
+	print json_encode($return);
+	mysqli_close($conn);
 }
-mysqli_close($conn);
 ?>
--- Projeto/index.php
+++ Projeto/index.php
@@ -5,21 +5,32 @@
   <?php include "./components/head.php" ?>
   <link rel="stylesheet" href="./styles/index.css">
   <title>SRV | Login</title>
+
+  <style>
+    #toast-container {
+      top: 10px !important;
+      right: auto !important;
+      bottom: 10%;
+      left: 50%;
+      transform: translate(-50%);
+      max-height: 250px;
+    }
+  </style>
 </head>
 
 <body>
   <div class="container">
     <h3 class="center">Login</h3>
     <p class="center"><i class="material-icons large">directions_car</i></p>
-    <form action="./config/login.php" method="POST">
+    <form id="form-login" method="POST">
       <div class="input-field">
         <i class="material-icons prefix">account_circle</i>
-        <input type="number" name="cpf" id="cpf" autocomplete="off" />
+        <input type="number" id="cpf" data-length="11" autocomplete="off" />
         <label for="cpf">CPF</label>
       </div>
       <div class="input-field">
         <i class="material-icons prefix">lock</i>
-        <input type="password" name="senha" id="senha" autocomplete="off"/>
+        <input type="password" id="senha" autocomplete="off"/>
         <label for="senha">Senha</label>
       </div>
       <button class="btn waves-effect" type="submit" name="submit">Entrar
@@ -27,9 +38,49 @@
       </button>
     </form>
   </div>
+  <form id="submit-form" action="./config/login.php" method="POST">
+    <input type="hidden" name="cpf" id="submit-cpf" />
+    <input type="hidden" name="senha" id="submit-senha" />
+    <input type="hidden" name="option" id="option" />
+  </form>
   <?php include "./components/scripts.php"; ?>
   <script>
     M.updateTextFields();
+    $(document).ready(function() {
+      $("#cpf").characterCounter();
+    });
+
+    $("#form-login").submit(function(e) {
+      e.preventDefault();
+      M.Toast.dismissAll();
+
+      let cpf = $("#cpf").val().trim();
+      let senha = $("#senha").val().trim();
+
+      $.ajax({
+        url: "./config/login.php",
+        type: "POST",
+        dataType: "json",
+        data: {
+          cpf,
+          senha,
+          option: "VERIFY"
+        },
+        success: function(response) {
+          if (response === "ok") {
+            $("#option").val("LOGIN");
+            $("#submit-cpf").val(cpf);
+            $("#submit-senha").val(senha);
+            $("#submit-form").submit();
+          } else {
+            M.toast({html: "Credenciais inválidas", classes: 'rounded #ef5350 red lighten-1'});
+          }
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      })
+    });
   </script>
 </body>
 
