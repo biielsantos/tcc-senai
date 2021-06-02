dd16ad0ffc483537968be72107c2b4858cc4b507
Luís Galete <luisgalete6264@gmail.com>
2021-06-02 11:43:49 -0300

    Atualizacao banco

--- Projeto/banco/script.sql
+++ Projeto/banco/script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2021 at 06:48 PM
+-- Generation Time: Jun 02, 2021 at 04:41 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.5
 
@@ -36,8 +36,10 @@ CREATE TABLE `departamento` (
 --
 
 INSERT INTO `departamento` (`id_departamento`, `departamento`) VALUES
-(1, 'Matadouro'),
-(2, 'TI');
+(1, 'TI'),
+(2, 'Vendas'),
+(3, 'Marketing'),
+(4, 'Suporte');
 
 -- --------------------------------------------------------
 
@@ -48,7 +50,11 @@ INSERT INTO `departamento` (`id_departamento`, `departamento`) VALUES
 CREATE TABLE `reserva` (
   `id_reserva` int(11) NOT NULL,
   `data_saida` datetime DEFAULT NULL,
+  `data_saida_real` datetime DEFAULT NULL,
   `data_retorno` datetime DEFAULT NULL,
+  `data_retorno_real` datetime DEFAULT NULL,
+  `km_saida` int(11) DEFAULT NULL,
+  `km_retorno` int(11) DEFAULT NULL,
   `status_reserva` tinytext DEFAULT NULL,
   `destino` varchar(150) DEFAULT NULL,
   `condutor` varchar(100) DEFAULT NULL,
@@ -62,11 +68,10 @@ CREATE TABLE `reserva` (
 -- Dumping data for table `reserva`
 --
 
-INSERT INTO `reserva` (`id_reserva`, `data_saida`, `data_retorno`, `status_reserva`, `destino`, `condutor`, `motivo`, `fk_id_departamento`, `fk_id_usuario`, `fk_id_veiculo`) VALUES
-(1, '2021-05-18 15:00:00', '2021-05-18 17:00:00', 'A', 'Prefeitura de Nova Esperança', 'Shaolin', 'Jogar truco com o prefeito', 1, 1, 4),
-(2, '2021-05-25 13:30:00', '2021-05-25 14:30:00', 'A', 'Algum lugar top', 'Big big', 'Fazer nada, como sempre', 2, 1, 1),
-(3, '2021-05-19 16:00:00', '2021-05-19 18:00:00', 'A', 'Catedral de Maringá', 'Papa Francisco', 'Rezar uma ave maria', 1, 1, 2),
-(4, '2021-05-19 08:00:00', '2021-05-19 09:00:00', 'A', 'Rua dos Coqueiros', 'Kleblão', 'Conversar com cliente', 2, 1, 1);
+INSERT INTO `reserva` (`id_reserva`, `data_saida`, `data_saida_real`, `data_retorno`, `data_retorno_real`, `km_saida`, `km_retorno`, `status_reserva`, `destino`, `condutor`, `motivo`, `fk_id_departamento`, `fk_id_usuario`, `fk_id_veiculo`) VALUES
+(1, '2021-06-02 12:00:00', NULL, '2021-06-02 13:00:00', NULL, NULL, NULL, 'A', 'Prefeitura de Nova Esperança', 'Shaolin', 'Jogar truco com o prefeito', 1, 1, 1),
+(2, '2021-06-03 18:00:00', NULL, '2021-06-03 19:00:00', NULL, NULL, NULL, 'A', 'Catedral de Maringá', 'Papa Francisco', 'Rezar uma ave maria', 4, 1, 2),
+(3, '2021-06-03 13:00:00', NULL, '2021-06-03 14:00:00', NULL, NULL, NULL, 'A', 'Alemanha', 'Ayrton Senna', 'Ganhar a F1', 4, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -91,11 +96,10 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nome`, `cpf`, `senha`, `tipo`, `telefone`, `validade_cnh`, `cnh`, `fk_id_departamento`) VALUES
-(1, 'Shaolin', '12345678901', 'c2hhb2xpbg==', 'A', '4498765432', '2024-05-15', '98765412365', 1),
-(2, 'Kleblão', '12345678902', 'a2xlYmxhbw==', 'U', '44998745687', '2024-06-19', '78945874547', 2),
-(3, 'Big Big', '12345678903', 'YmlnYmln', 'U', '44998745487', '2024-10-23', '12365478542', 2),
-(4, 'Rodrigo Pato', '12345678904', 'cm9kcmlnbw==', 'A', '44998758789', '2025-06-18', '98745874587', 2),
-(5, 'Gabriel', '12345678910', 'Z2FicmllbA==', 'A', '44998754874', '2024-02-17', '12345687878', 2);
+(1, 'Shaolin', '12345678901', 'c2hhb2xpbg==', 'A', '44132131232', '2027-06-18', '15649849489', 1),
+(2, 'Kleblão', '12345678902', 'a2xlYmxhbw==', 'U', '44858686868', '2029-06-14', '47237472347', 1),
+(3, 'Rodrigo Pato', '46545465456', 'cm9kcmlnbw==', 'A', '44998758789', '2035-06-15', '54645464646', 1),
+(4, 'Big Big', '45646546548', 'YmlnYmln', 'U', '44766745645', '2035-06-14', '78484848484', 3);
 
 -- --------------------------------------------------------
 
@@ -116,11 +120,10 @@ CREATE TABLE `veiculo` (
 --
 
 INSERT INTO `veiculo` (`id_veiculo`, `modelo`, `placa`, `proprietario`, `status_veiculo`) VALUES
-(1, 'Patinete Xiaomi', 'ASD4568', 'Xi Jinping', 'A'),
-(2, 'Monza 89', 'AST4587', 'Carlão', 'A'),
-(3, 'Cavalo Mecânico', 'ASH9898', 'Sergão', 'A'),
-(4, 'Corsa Rebaixado', 'DRF8724', 'Rodrigo Pato', 'A'),
-(5, 'Fusca Azul', 'AVH4587', 'Ayrton Senna', 'A');
+(1, 'Monza 89', 'ADS1343', 'Kleber Pedra', 'A'),
+(2, 'Patinete Xiaomi', 'AGH4323', 'Xi Jinping', 'A'),
+(3, 'Fusca Azul', 'ASM5465', 'Ayrton Senna', 'A'),
+(4, 'Corsa Rebaixado', 'ASD8676', 'Rogerinho', 'A');
 
 --
 -- Indexes for dumped tables
@@ -161,25 +164,25 @@ ALTER TABLE `veiculo`
 -- AUTO_INCREMENT for table `departamento`
 --
 ALTER TABLE `departamento`
-  MODIFY `id_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `veiculo`
 --
 ALTER TABLE `veiculo`
-  MODIFY `id_veiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_veiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
